52e29c89ea0d86295dc4973c217cb25d3607e469
ashram7 <ashram7@hanmail.net>
2023-07-10 14:14:39 +0900

    lesson04

--- SpringDataJDBCSample/src/main/java/com/hschool/SpringDataJDBCSample/entity/Member.java
+++ SpringDataJDBCSample/src/main/java/com/hschool/SpringDataJDBCSample/entity/Member.java
@@ -1,9 +1,9 @@
 package com.hschool.SpringDataJDBCSample.entity;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data //클래스에 부여하는 것, 전 필드에 대해 getter/setter를 액세스 할 수 있다. hashCode(), equals(), toString() 도 사용가능
 @NoArgsConstructor //클래스에 부여하는 것, 기본 생성자가 자동 생성됨
--- SpringDataJDBCSample/src/main/java/com/hschool/SpringDataJDBCSample/repository/MemberCrudRepository.java
+++ SpringDataJDBCSample/src/main/java/com/hschool/SpringDataJDBCSample/repository/MemberCrudRepository.java
@@ -4,5 +4,5 @@ import com.hschool.SpringDataJDBCSample.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MemberCrudRepository extends CrudRepository<Member, Integer> {
-    
+
 }
