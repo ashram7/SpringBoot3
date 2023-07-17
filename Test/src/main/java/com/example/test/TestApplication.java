7f999b8a61187609600a90f069b282ddeda05364
ashram7 <ashram7@hanmail.net>
2023-07-17 12:15:50 +0900

    lesson11

--- Test/src/main/java/com/example/test/TestApplication.java
+++ Test/src/main/java/com/example/test/TestApplication.java
@@ -24,11 +24,11 @@ public class TestApplication {
 
 	private void execute() {
 		// 등록 처리
-		//setup();
+		setup();
 		// 전체 항목 취득
-		//showList();
+		showList();
 		// 1개 항목 취득
-		//showOne();
+		showOne();
 		//변경처리
 		//updateTest();
 		//삭제처리
@@ -67,7 +67,7 @@ public class TestApplication {
 
 	private void showOne() {
 		System.out.println("--- １건 취득 개시 ---");
-		Optional<Test> testOpt = repository.findById(1);
+		Optional<Test> testOpt = service.selectOneById(1);
 		if (testOpt.isPresent()) {
 			System.out.println(testOpt.get());
 		} else {
@@ -79,14 +79,32 @@ public class TestApplication {
 	private void updateTest() {
 		System.out.println("--- 변경 처리 개시 ---");
 		Test test1 = new Test(1,"JVM은 운영체제별로 동일한 JVM이 사용된다.", false, "홍길동");
-		test1 = repository.save(test1);
+		service.updateTest(test1);
 		System.out.println("변경된 데이터는 " + test1 + "입니다.");
 		System.out.println("--- 변경 처리 완료 ---");
 	}
 
 	private void deleteTest() {
 		System.out.println("--- 삭제 처리 개시 ---");
-		repository.deleteById(2);
+		service.deleteTestById(2);
 		System.out.println("--- 삭제 처리 완료 ---");
+	}
+
+	private void doTest() {
+		System.out.println("--- 퀴즈 1건 취득 개시 ---");
+		Optional<Test> testOpt = service.selectOneRandomTest();
+		if (testOpt.isPresent()) {
+			System.out.println(testOpt.get());
+		} else {
+			System.out.println("해당 데이터는 존재하지 않습니다.");
+		}
+		System.out.println("--- 퀴즈 １건 취득 완료 ---");
+		Boolean myAnswer = false;
+		Integer id = testOpt.get().getId();
+		if (service.checkTest(id, myAnswer)) {
+			System.out.println("정답입니다!!!");
+		} else {
+			System.out.println("오답입니다.");
+		}
 	}
 }
