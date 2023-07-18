8e0e8576aa6b901e301b167ab0697c957ebceb2e
ashram7 <ashram7@hanmail.net>
2023-07-18 11:35:05 +0900

    lesson12

--- Test/src/main/java/com/example/test/controller/TestController.java
+++ Test/src/main/java/com/example/test/controller/TestController.java
@@ -6,9 +6,13 @@ import com.example.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/test")
@@ -27,12 +31,35 @@ public class TestController {
     //Test 목록 표시
     @GetMapping
     private String showList(TestForm testForm, Model model) {
-        //신규등록 설정
+        //신규 등록 설정(요부분)
+        testForm.setNewTest(true);
+        //퀴즈 목록 취득
         Iterable<Test> list = service.selectAll();
         //표시용 Model에 저장
         model.addAttribute("list", list);
         model.addAttribute("title","등록 폼");
         return "crud";
+    }
+
+    @PostMapping("/insert")
+    public String insert(@Validated TestForm testForm, BindingResult bindingResult,
+                         Model model, RedirectAttributes redirectAttributes){
+        // Form에서 Entity로 넣기
+        Test test = new Test();
+        test.setQuestion(testForm.getQuestion());
+        test.setAnswer(testForm.getAnswer());
+        test.setAuthor(testForm.getAuthor());
+
+        // 입력 체크
+        if (!bindingResult.hasErrors()) {
+            service.insertTest(test);
+            redirectAttributes.addFlashAttribute("complete", "등록이 완료되었습니다");
+            return "redirect:/test";
+        } else {
+            // 에러가 발생한 경우에는 목록 표시로 변경
+            return showList(testForm, model);
+        }
+
     }
 
 }
